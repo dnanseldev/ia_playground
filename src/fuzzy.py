4ac588e5fc8a899e3eaba7e89da66d546eb2d1aa
dnanseldev <dnansel.dev@gmail.com>
2024-04-12 15:46:54 -0300

    More changes

--- src/fuzzy.py
+++ src/fuzzy.py
@@ -4,9 +4,11 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-print(np.arange(0,11,1))
+#print(np.arange(0,11,1))
 
 quality = ctrl.Antecedent( np.arange(0,11,1), 'Quality' )
 service = ctrl.Antecedent( np.arange(0,11,1), 'Service' )
 
-print("Hello Fuzzy")
+print(quality)
+print(quality.universe)
+
