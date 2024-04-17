ec632b5c9b4f761278a95ac26c88d66dee40fdf5
dnanseldev <dnansel.dev@gmail.com>
2024-04-16 22:55:16 -0300

    Improving in fuzzy logic

--- src/fuzzy.py
+++ src/fuzzy.py
@@ -7,7 +7,10 @@ from skfuzzy import control as ctrl
 #print(np.arange(0,11,1))
 
 quality = ctrl.Antecedent( np.arange(0,11,1), 'Quality' )
+quality.automf(number=3, names=['bad', 'good', 'delicious'])
+
 service = ctrl.Antecedent( np.arange(0,11,1), 'Service' )
+service.automf(number=3, names=['bad', 'acceptable', 'excellent'])
 
 print(quality)
 print(quality.universe)
@@ -15,3 +18,4 @@ print(quality.universe)
 
 tip = ctrl.Consequent(np.arange(0,21,1), 'Tip')
 print(tip.universe)
+print(quality.view())
